4d717468924c8809c39f04d033ccce1a27fbcf84
Charles Sipe <csipe84@gmail.com>
2015-04-28 23:16:04 -0700

    Added priority select box

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -1,18 +1,63 @@
 'use strict';
 
-angular.module('destructingTaskListApp')
+angular.module('destructingTaskListApp') //,["firebase"] injecting firebase causes app to break
   .controller('MainCtrl', function ($scope, $http) {
-    $scope.awesomeThings = [];
+    $scope.awesomeThings = [
+      {done: false, name:'Eat carrots'},
+      {done: false, name: 'Run 2 miles'},
+      {done: false, name: 'Do 100 pushups'},
+    ];
+
+    var ref = new Firebase("https://glowing-heat-9383.firebaseio.com/"); // Instantiate the Firebase service with the new operator.
+
+    // download the data into a local object
+    //$scope.data = $firebaseObject(ref);
+
+    // create a synchronized array
+    //$scope.tasks = $firebaseArray(ref);
+
+    $scope.addTodo = function() {   
+      var newTodo = {
+      done: false,
+      name: $scope.todoText
+      };
+
+      $scope.exampleTasks.push(newTodo);  // Push input onto array
+    
+      $scope.todoText = '';  // Erases the input after submit
+    }
+
+    /* $scope.hideOldTasks = function() {
+      if (exampleTasks.timestamp > 7 days) {
+        nghide = true;
+      } 
+
+      else {
+        nghide = false;
+      }
+
+    };
+    */
+
+    $scope.todoItems = [
+      {done: false, name: 'Set a goal'}
+    ];
 
     // Example tasks to show in view
     $scope.exampleTasks = [
-    'Buy milk',
-    'Check the mail',
-    'Practice JavaScript'
+      {done: false, name:'Buy milk'},
+      {done: false, name: 'Check the mail'},
+      {done: false, name: 'Practice JavaScript'},
     ];
 
+    
     $http.get('/api/things').success(function(awesomeThings) {
       $scope.awesomeThings = awesomeThings;
     });
 
   });
+
+
+//['firebase','ui.router'] 
+
+//, $firebaseObject
--- client/app/main/main.js
+++ client/app/main/main.js
@@ -8,4 +8,12 @@ angular.module('destructingTaskListApp')
         templateUrl: 'app/main/main.html',
         controller: 'MainCtrl'
       });
+
+    $stateProvider
+      .state('completed', {  // Completed tasks view
+        url: '/completed',
+        templateUrl: 'app/main/completed.html',
+        controller: 'MainCtrl',
+    });
+
   });
